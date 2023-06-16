31086e203116ade5065a60c970e0def0ff11efcf
DmySyGit <dmi.sy.2000@gmail.com>
2023-06-16 12:42:30 +0300

    чистка кода

--- ParsingToyRuConsoleApp/Program.cs
+++ ParsingToyRuConsoleApp/Program.cs
@@ -11,22 +11,6 @@ namespace ParsingToyRuConsoleApp
         
         static void Main(string[] args)
         {
-
-
-            //using (CsvToyRu ds = new CsvToyRu())
-            //{
-
-            //    //    for (int i = 0; i < 1000000; i++)
-            //    //    {
-            //    Product product = new Product();
-            //    product.Name = "dsf";
-            //    product.LastPrice = "121";
-            //    product.ImagesLinks = string.Join("     ", new List<string> { "dfs", "dfs", "2312" });
-            //    ds.AddRecord(product);
-            //    //    }
-            //}
-            //Console.WriteLine(AngleSharpToyRu.GetPageHtml("https://www.toy.ru/catalog/boy_transport/", false));
-
             List<Product> productsMoskow = AngleSharpToyRu.GetBoyTransportProducts();
             using (CsvToyRu ds = new CsvToyRu())
             {
@@ -35,13 +19,7 @@ namespace ParsingToyRuConsoleApp
                     ds.AddRecord(product);
                 }
             }
-            //    Console.WriteLine("Для Москвы товаров: "+linksProductsMoskow.Count());
-            //    foreach (string item in linksProductsMoskow)
-            //    {
-
-            //    }
             Console.WriteLine("End");
-
         }
     }
 }
